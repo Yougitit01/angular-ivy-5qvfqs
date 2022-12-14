611b0aafeb81c4880c230f612485b561a6b45bae
Yougitit01 <119706018+Yougitit01@users.noreply.github.com>
2022-12-14 12:02:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-5qvfqs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-5qvfqs)
---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment 2</title>
+    
+</head>
+<body>
+    <h1>Mozilla</h1>
+            <td>
+                <tc>
+                    <tr>
+                        <img align="right"src="Logo 2.png" alt="image" width="200px" height="150">
+                    </tr>
+                </tc>
+            </td>
+        <br>
+            <iframe width="400" height="225" src="https://www.youtube.com/embed/ojcNcvb1olg" frameborder="0" allowfullscreen></iframe>
+    <h2>Rocking the free web</h2>
+    <br>
+    <p>Mozilla are a global community of technologist, thinkers , and builders, working together to keep the internet alive and accessible, so people worldwide can be informed contributors and creators of the web. We believe this act of human collaboration across an open platform is essential to individual growth and our collective future.</p>
+    <br>
+    <p>Click on the the imgaes below to find more information about the cool stuff Mozilla does. <a href =https://www.bing.com/images/search?view=detailV2&ccid=dNIxU3Gl&id=8181B881EEC2BE8772845F0B2B882260BA1731C2&thid=OIP.dNIxU3GlJFXw9fn_a6HpCAHaE8&mediaurl=https%3a%2f%2fwpcluster.dctdigital.com%2fpressandjournal%2fwp-content%2fuploads%2fsites%2f2%2f2018%2f11%2f46488322_10156214796388547_2646235260664152064_n1.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.74d2315371a52455f0f5f9ff6ba1e908%3frik%3dwjEXumAiiCsLXw%26pid%3dImgRaw%26r%3d0&exph=640&expw=960&q=red+panda&simid=608049515747102979&FORM=IRPRST&ck=0B9D2F70C390FC3DBCD9E8EB9CB22778&selectedIndex=52&ajaxhist=0&ajaxserp=0> Red panda Picture</a></p>
+    <br>
+    <td>
+        <tr>
+            <tc>
+                <img src="Logo 2.png" alt="image" width="200px" height="150px">
+                <img src="logo 4.png" alt="image" width="200px" height="150px">
+                <img src="logo 1.jpg" alt="image" width="200px" height="150px">
+                <img src="Logo 3.svg" alt="image" width="160px" height="150px">
+            </tc>
+        </tr>
+    </td>
+    <br>
+    <td>
+        <tr>
+            <img src="panda.jpg" alt="image" width="800px">
+
+        </tr>
+    </td> 
+</body>
+</html>
